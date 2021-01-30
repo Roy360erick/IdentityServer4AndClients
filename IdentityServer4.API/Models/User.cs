3312dfa093c68a208213721d0b036dd6e48c5fcd
Erick Roy Fernandez Terrel <erick.fernandez@tecsup.edu.pe>
2021-01-29 22:12:22 -0500

    first commit

---
+++ IdentityServer4.API/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace IdentityServer4.API.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+    }
+}
---
+++ IdentityServer4.MvcClient/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace IdentityServer4.MvcClient.Models
+{
+    public class UserModel
+    {
+        public UserModel()
+        {
+        }
+    }
+}
